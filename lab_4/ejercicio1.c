85a206b4f8b64a49d8422f77cc737be75266e2a4
George <george.morison@ucr.ac.cr>
2025-10-22 08:50:12 -0600

    Se implemento el algoritmo de ordenamiento y se termino con el ejercicio

--- lab_4/ejercicio1.c
+++ lab_4/ejercicio1.c
@@ -31,19 +31,53 @@ ejecucion del programa, entonces es una forma facil de lograrlo*/
     }
     
 
-    printf("la matriz corresponde a:\n");
+    printf("Matriz: \n");
     for (int i = 0; i < Filas; i++) {
         for (int j = 0; j < Columnas; j++) {
             printf("%d ", matriz[i][j]);
         }
         printf("\n");
     }
+printf ("\n");
 
 //convertimos la matriz en un vector con los punteros
     printf("Vector:\n");
 for (int i = 0; i < Filas * Columnas; i++) {
     *(ptrVector + i) = *(ptrMatriz + i);
 }
+
+//Ordenamos el vector de menor a mayor con "Bubble sort"
+
+    for (int i = 0; i < Filas * Columnas - 1; i++) {
+        for (int j = 0; j < Filas * Columnas - i - 1; j++) {
+            if (*(ptrVector + j) > *(ptrVector + j + 1)) {
+                int temp = *(ptrVector + j);
+                *(ptrVector + j) = *(ptrVector + j + 1);
+                *(ptrVector + j + 1) = temp;
+            }
+        }
+    }
+printf ("\n");
+//Imprimimos el vector ya ordenado para verifica que se hizo correctamente
+
+for (int i = 0; i < Filas * Columnas; i++) {
+        printf("%d ", *(ptrVector + i));
+    }
+
+//Volvemos a convertir el arreglo en una matriz ordenada de menor a mayor
+for (int i = 0; i < Filas * Columnas; i++) {
+        *(ptrMatriz + i) = *(ptrVector + i);
+    }
+
+printf ("\n");
+//Imprimimos la matriz
+printf("Matriz ordenada:\n");
+    for (int i = 0; i < Filas; i++) {
+        for (int j = 0; j < Columnas; j++) {
+            printf("%d ", *(*(matriz + i) + j));
+        }
+        printf("\n");
+    }
 
 
 return 0;
